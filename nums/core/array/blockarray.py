3f3aa6510a9a9396bfb6a920943fc7ffc2fa3066
Melih Elibol <elibol@gmail.com>
2021-04-24 13:31:31 -0700

    test tensordot

--- nums/core/array/blockarray.py
+++ nums/core/array/blockarray.py
@@ -477,27 +477,28 @@ class BlockArray(BlockArrayBase):
         # Even with _compute_tensordot_grid_args,
         # retaining the below is beneficial.
 
-        def basic_vector(ba: BlockArray, axis):
-            if len(ba.shape) == 0:
-                return False
-            if len(ba.shape) == 1:
-                return True
-            size = ba.shape[axis]
-            rest = list(ba.shape[:axis]) + list(ba.shape[axis + 1:])
-            return np.sum(rest) == len(rest) <= 1 < size
-
-        other = self.check_or_convert_other(other)
-        if basic_vector(self, len(self.shape) - 1) and basic_vector(other, 0):
-            return self._vecdot(other)
-        elif len(self.shape) == 2 and (len(other.shape) == 1
-                                       or (len(other.shape) == 2 and other.shape[1] == 1)):
-            # Optimized matrix vector multiply.
-            return self._matvec(other)
-        else:
-            return self._tensordot(other, axes)
+        # def basic_vector(ba: BlockArray, axis):
+        #     if len(ba.shape) == 0:
+        #         return False
+        #     if len(ba.shape) == 1:
+        #         return True
+        #     size = ba.shape[axis]
+        #     rest = list(ba.shape[:axis]) + list(ba.shape[axis + 1:])
+        #     return np.sum(rest) == len(rest) <= 1 < size
+        #
+        # other = self.check_or_convert_other(other)
+        # if basic_vector(self, len(self.shape) - 1) and basic_vector(other, 0):
+        #     return self._vecdot(other)
+        # elif len(self.shape) == 2 and (len(other.shape) == 1
+        #                                or (len(other.shape) == 2 and other.shape[1] == 1)):
+        #     # Optimized matrix vector multiply.
+        #     return self._matvec(other)
+        # else:
+        return self._tensordot(other, axes)
 
     def _compute_tensordot_grid_args(self, self_block: Block, other_block: Block):
-        if np.prod(self_block.shape) < np.prod(other_block.shape):
+        # Schedule on the block that is larger.
+        if np.prod(self_block.shape) >= np.prod(other_block.shape):
             if self_block.transposed:
                 return tuple(reversed(self_block.grid_entry)), self_block.grid_shape
             else:
